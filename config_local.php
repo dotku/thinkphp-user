2f0873292652aa9d3285561d05d7b619dd80f90f
dotku <weijingjaylin@gmail.com>
2016-01-28 06:58:48 +0800

    update config_local.php

--- config_local.php
+++ config_local.php
@@ -8,11 +8,20 @@
         'lib_thinkphp' => './thinkphp_3.2.3/ThinkPHP.php',
         
          // database
+         /*
+         hostname: 10.9.1.188
+         jdbcUrl: jdbc:mysql://10.9.1.188:3306/cf_3a0c73e9_e71c_46eb_8dce_b09350405800?user=zyMN5R9FebiJKxWS&password=JHyknPfY0Q8FmBKs
+         name: cf_3a0c73e9_e71c_46eb_8dce_b09350405800
+         password: JHyknPfY0Q8FmBKs
+         port: 3306
+         uri: mysql://zyMN5R9FebiJKxWS:JHyknPfY0Q8FmBKs@10.9.1.188:3306/cf_3a0c73e9_e71c_46eb_8dce_b09350405800?reconnect=true
+         username: zyMN5R9FebiJKxWS
+         */
         'DB_TYPE'   => 'mysql', // 数据库类型
-        'DB_HOST'   => 'localhost', // 服务器地址
+        'DB_HOST'   => '10.9.1.188', // 服务器地址
         'DB_NAME'   => 'thinkphp_user', // 数据库名
-        'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'admin123', // 密码
+        'DB_USER'   => 'zyMN5R9FebiJKxWS', // 用户名
+        'DB_PWD'    => 'JHyknPfY0Q8FmBKs', // 密码
         'DB_PORT'   => 3306, // 端口
         'DB_CHARSET'=> 'utf8', // 字符集
     );
